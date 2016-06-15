07bcb3b2cd1a344030c64e8b68fcf2c7e7af2ee1
silver--stoun <shtirlict@yandex.ru>
2016-06-15 19:07:23 +0400

    Сделал отрисовку змейки

--- snake/main.cpp
+++ snake/main.cpp
@@ -7,6 +7,12 @@ int Scale = 25;
 int w = Scale * N;
 int h = Scale * M;
 
+int dir, num = 4;
+
+struct {
+	int x;
+	int y; } s[100];
+
 void DrawField() {
 	glColor3f(0.0, 0.0, 0.0);
 
@@ -26,10 +32,32 @@ void DrawField() {
 	glEnd();
 }
 
+void Tick() {
+	for (int i = num; i > 0; --i)
+	{
+		s[i].x = s[i - 1].x;
+		s[i].y = s[i - 1].y;
+	}
+
+	if (dir == 0) s[0].y += 1;
+	if (dir == 1) s[0].x -= 1;
+	if (dir == 2) s[0].x += 1;
+	if (dir == 3) s[0].y -= 1;
+}
+
+void DrawSnake() {
+	glColor3f(0.0, 0.0, 0.0);
+	for (int i = 0; i < num; i++)
+	{
+		glRectf(s[i].x * Scale + 2, s[i].y * Scale + 2, (s[i].x + 1) * Scale, (s[i].y + 1) * Scale);
+	}
+}
+
 void display() {
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	DrawField();
+	DrawSnake();
 
 	glFlush();
 }
@@ -37,11 +65,15 @@ void display() {
 void timer(int = 0) {
 	display();
 
+	Tick();
 
-	glutTimerFunc(50, timer, 0);
+	glutTimerFunc(250, timer, 0);
 }
 
 int main(int argc, char **argv) {
+
+	s[0].x = 10;
+	s[0].y = 0;
 
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
