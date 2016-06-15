d881104c071c8e26074ec60ae5a9855ae92b3094
silver--stoun <shtirlict@yandex.ru>
2016-06-15 18:39:37 +0400

    Пробная анимация

--- snake/main.cpp
+++ snake/main.cpp
@@ -2,19 +2,28 @@
 #include <GL\glut.h>
 
 int w = 640, h = 480;
+int i = 0;
 
 void display() {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glColor3f(1.0, 0.0, 0.0);
 
 	glBegin(GL_QUADS);
-	glVertex2f(50, 50);
-	glVertex2f(100, 50);
-	glVertex2f(100, 100);
-	glVertex2f(50, 100);
+	glVertex2f(50 + i, 50);
+	glVertex2f(100 + i, 50);
+	glVertex2f(100 + i, 100);
+	glVertex2f(50 + i, 100);
 	glEnd();
 
 	glFlush();
+}
+
+void timer(int = 0) {
+	display();
+
+	i++;
+
+	glutTimerFunc(50, timer, 0);
 }
 
 int main(int argc, char **argv) {
@@ -28,6 +37,7 @@ int main(int argc, char **argv) {
 	gluOrtho2D(0, w, 0, h);
 
 	glutDisplayFunc(display);
+	glutTimerFunc(50, timer, 0);
 
 	glutMainLoop();
 }
