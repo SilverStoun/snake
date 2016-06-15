8af2f216da12bf9ae22cb90cd479cd9b81e96c6d
silver--stoun <shtirlict@yandex.ru>
2016-06-15 18:54:41 +0400

    Отрисовка поля

--- snake/main.cpp
+++ snake/main.cpp
@@ -1,19 +1,35 @@
 #include <time.h>
 #include <GL\glut.h>
 
-int w = 640, h = 480;
-int i = 0;
+int N = 30, M = 20;
+int Scale = 25;
+
+int w = Scale * N;
+int h = Scale * M;
+
+void DrawField() {
+	glColor3f(0.0, 0.0, 0.0);
+
+	glBegin(GL_LINES);
+
+	for (int i = 0; i < w; i+= Scale)
+	{
+		glVertex2f(i, 0);
+		glVertex2f(i, h);
+	}
+	for (int j = 0; j < h; j+= Scale)
+	{
+		glVertex2f(0, j);
+		glVertex2f(w, j);
+	}
+
+	glEnd();
+}
 
 void display() {
 	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(1.0, 0.0, 0.0);
 
-	glBegin(GL_QUADS);
-	glVertex2f(50 + i, 50);
-	glVertex2f(100 + i, 50);
-	glVertex2f(100 + i, 100);
-	glVertex2f(50 + i, 100);
-	glEnd();
+	DrawField();
 
 	glFlush();
 }
@@ -21,7 +37,6 @@ void display() {
 void timer(int = 0) {
 	display();
 
-	i++;
 
 	glutTimerFunc(50, timer, 0);
 }
@@ -31,7 +46,8 @@ int main(int argc, char **argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowSize(w, h);
-	glutCreateWindow("Test");
+	glutCreateWindow("Snake");
+	glClearColor(0.5, 0.5, 0.5, 1.0);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluOrtho2D(0, w, 0, h);
